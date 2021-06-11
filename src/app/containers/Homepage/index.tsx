a7bd34711199691b47df2986be57f68d1e6a495e
Inamul Hassan <mfihassan99@gmail.com>
2021-06-11 20:07:49 +0530

    setting up tailwind css

---
+++ src/app/containers/Homepage/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+import tw from 'twin.macro';
+
+const PageContainer = styled.div`
+  ${tw`
+    flex
+    flex-col
+    w-full
+    h-full
+    items-center
+    overflow-x-hidden
+  `}
+`;
+
+interface Props {}
+
+const HomePage = (props: Props) => {
+  return <div></div>;
+};
+
+export default HomePage;
